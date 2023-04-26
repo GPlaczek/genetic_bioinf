760a859e9623d92de619ab4a729eb47b0b906f76
Grzegorz Płaczek <g01.placzek@gmail.com>
2023-04-26 15:39:17 +0200

    Generate initial population

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,9 @@
 #include <getopt.h>
 #include <memory>
 #include <fstream>
+#include <numeric>
+#include <algorithm>
+#include <random>
 
 #include "config.hpp"
 #include "instance.hpp"
@@ -10,6 +13,26 @@ const static struct option long_opts[] = {
     {"config", required_argument, NULL, 'c'},
     {0, 0, 0, 0},
 };
+
+void genetic(
+    Config &config,
+    Instance &instance
+) {
+    std::default_random_engine rng{};
+    std::vector<std::vector<int>> population(
+        config.population.size,
+        std::vector<int>(instance.getNWords()));
+
+    // Generate initial population
+    for (
+        std::vector<std::vector<int>>::iterator it = population.begin();
+        it < population.end();
+        it++
+    ) {
+        std::iota(it->begin(), it->end(), 0);
+        std::shuffle(it->begin(), it->end(), rng);
+    }
+}
 
 int main(int argc, char *argv[]) {
     std::unique_ptr<std::istream> in_cfg;
@@ -35,9 +58,11 @@ int main(int argc, char *argv[]) {
             optind++;
         }
     }
-    Config c = Config(std::move(in_cfg));
+    Config c(std::move(in_cfg));
 
-    Instance i = Instance(std::move(in_inst), 10, 5);
+    Instance i(std::move(in_inst), 10, 5);
+
+    genetic(c, i);
 
     return 0;
 }
